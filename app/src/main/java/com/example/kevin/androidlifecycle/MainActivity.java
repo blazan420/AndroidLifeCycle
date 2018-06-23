615719a09fe627e4d84e7e6c9267a161b54b0f2f
kevin <kevinjobrien420@gmail.com>
2018-06-23 02:38:51 -0500

    244 AndroidLifeCycle // Complete

--- app/src/main/java/com/example/kevin/androidlifecycle/MainActivity.java
+++ app/src/main/java/com/example/kevin/androidlifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kevin.androidlifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"The onCreate Method is Called", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart Method is called",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume Method called", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause Method is called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart Method is Called",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy Method is called", Toast.LENGTH_LONG).show();
     }
 }
